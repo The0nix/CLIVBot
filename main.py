685ba283ef8fb90afeebb884c11dd8dac2d2a5dd
The0nix <nektonikto999@gmail.com>
2016-08-04 14:41:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+BOT_TOKEN = "234891684:AAFBx9W0Ona57eE7_hDSPSowQMib5L4vzJ4"
+URL = "https://api.telegram.org/bot{0}/".format(BOT_TOKEN)
+MyURL = "https://example.com/hook"
+
+api = requests.Session()
+application = tornado.web.Application([
+    (r"/", Handler),
+])
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, signal_term_handler)
+    try:
+        set_hook = api.get(URL + "setWebhook?url=%s" % MyURL)
+        if set_hook.status_code != 200:
+            logging.error("Can't set hook: %s. Quit." % set_hook.text)
+            exit(1)
+        application.listen(8888)
+        tornado.ioloop.IOLoop.current().start()
+    except KeyboardInterrupt:
+        signal_term_handler(signal.SIGTERM, None)
